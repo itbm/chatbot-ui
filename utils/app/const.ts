1366fcad31906f77118e03f403f716a0e5d3fb00
itbm <22393016+itbm@users.noreply.github.com>
2023-06-10 12:59:57 +0100

    Update default temperature and prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,12 +1,12 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using British English and markdown.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
 export const DEFAULT_TEMPERATURE = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
+  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.2");
 
 export const OPENAI_API_TYPE =
   process.env.OPENAI_API_TYPE || 'openai';
